3cd6326ffbf02f978765f2baaf81596e4d1c5c49
hrithiksonar <107844855+hrithiksonar@users.noreply.github.com>
2022-06-23 22:05:52 +0530

    Update BookCricketGame.java

--- BookCricketGame.java
+++ BookCricketGame.java
@@ -18,12 +18,12 @@ public class BookCricketGame
      System.out.println("6. Show the winner of the game");
      while(true)
      {
-           int p1_score=0,p2_score=0,points,p1_round=0,p2_round=0,pg;
+           int p1_score=0,p2_score=0,points,p1_round=0,p2_round=0,pageNum;
            System.out.print("Enter 1 To Start Game And 0 For Exit : ");
            choice=sc.nextInt();
            if(choice==0)
            {
-             break;
+               break;
            }
            System.out.print("Enter Player 1 Name : ");
            p1_name=sc.next();
@@ -32,49 +32,52 @@ public class BookCricketGame
            System.out.println("Player : "+p1_name);
            while(true)
            {
-           System.out.print("Enter 1 To Open Book : ");
-           int order=sc.nextInt();
-           pg=(int)(Math.random()*(300)+1);
-           points=pg%7;
-           p1_score+=points;
-
-             p1_round++;
-             if(points==0)
-             {
-             System.out.println("!!!\t\tOPPS\t\t!!!");
-             }
-             System.out.println("Page Number "+pg+"\t\t\tPoint : "+points+"\t\t\tScore : "+p1_score);
-             if(points==0)
-             {
-               System.out.println(p1_name+" Total Score : "+p1_score);
-               System.out.println(p1_name+" Takes "+p1_round+" Rounds ");
-               break;
-             }
+                System.out.print("Enter 1 To Open Book : ");
+                int order=sc.nextInt();
+                pageNum=(int)(Math.random()*(300)+1);
+                points=pageNum%7;
+                p1_score+=points;
+                p1_round++;
+                if(points==0)
+                {
+                     System.out.println("!!!\t\tOPPS\t\t!!!");
+                }
+                System.out.println("Page Number "+pageNum+"\t\t\tPoint : "+points+"\t\t\tScore : "+p1_score);
+                if(points==0)
+                {
+                     System.out.println(p1_name+" Total Score : "+p1_score);
+                     System.out.println(p1_name+" Takes "+p1_round+" Rounds ");
+                     break;
+                }
            }
            System.out.println("\n"+p2_name+" Needs "+(p1_score+1)+" Score In "+p1_round+" Rounds To WIN MATCH\nPlayer : "+p2_name);
            p2_round=p1_round;
            while(true)
            {
-           System.out.print("Enter 1 To Open Book : ");
-           int order=sc.nextInt();
-           pg=(int)(Math.random()*(300)+1);
-           points=pg%7;
-           p2_score+=points;
-          System.out.println("Page Number "+pg+"\t\t\tPoint : "+points+"\t\t\tScore : "+p2_score+"\t\t\tRounds Left : "+(--p2_round));
-           if(points==0 || p2_round==0 || p2_score>p1_score)
-             {
-               System.out.println();
-               break;
-             }
+                System.out.print("Enter 1 To Open Book : ");
+                int order=sc.nextInt();
+                pageNum=(int)(Math.random()*(300)+1);
+                points=pageNum%7;
+                p2_score+=points;
+                System.out.println("Page Number "+pageNum+"\t\t\tPoint : "+points+"\t\t\tScore : "+p2_score+"\t\t\tRounds Left : "+(--p2_round));
+                if(points==0 || p2_round==0 || p2_score>p1_score)
+                {
+                      System.out.println();
+                      break;
+                }
            }
            if(p2_score>p1_score || ((p2_score==p1_score) && (p2_round<p1_round)))
            {
-             System.out.print(p2_name+" Win Match !!\n\n");
+                System.out.print(p2_name+" Win Match !!\n\n");
            }
-           else
+           else if(p1_score>p2_score)
            {
-             System.out.print(p1_name+" Win Match !!\n\n");
+                System.out.print(p1_name+" Win Match !!\n\n");
            }
+		     else
+			  {
+				  	 System.out.print("IT'S A TIE");
+			  }
        }
    }
 }
